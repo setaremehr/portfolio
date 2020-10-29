9ac2f59909739629d80ff40c3956d8ffc78db252
setare mehr <setaremehr89@gmail.com>
2020-10-29 02:38:34 -0700

    link the project info to the card

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import ProjectInfo from './ProjectInfo';
 function Card(props){
 
     return(
-     <div className="d-inline-block">
+     <div className="d-inline-block proCard" onClick={(e) => props.click(props.item)}>
          <img className='proCard-image' src={props.item.img} alt={props.item.img} />
          { props.item.selected && <ProjectInfo title={props.item.title} link={props.item.link} /> }
      </div>
