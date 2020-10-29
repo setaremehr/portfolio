07b278e33574eff2e328b678454168956784a092
setare mehr <setaremehr89@gmail.com>
2020-10-29 01:48:01 -0700

    fixed background view

--- src/components/BackImage.js
+++ src/components/BackImage.js
@@ -9,13 +9,13 @@ import Media from 'react-bootstrap/Media'
 function BackImage (props) {
 
     return(
-        <Media>
+        <Media className="justify-content-center mt-3"> 
   <img
     width={350}
     height={300}
     className="ml-5 mt-3 mr-3 "
-    src="https://thumbs.dreamstime.com/b/people-communicating-colorful-silhouettes-holding-speech-balloons-communication-concept-white-background-40256743.jpg"
-    alt="Generic placeholder"
+    src="https://img.pngio.com/about-me-png-download-1100550-free-transparent-logo-png-download-about-me-png-900_460.jpg"
+    alt="myPic"
   />
   <Media.Body className="justify-content-center py-5 ml-5 mt-5">
     <h5 >Media Heading</h5>
