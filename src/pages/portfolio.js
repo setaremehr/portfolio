192f77cf6da724422a2ed9809eb64953bcdebbd9
setare mehr <setaremehr89@gmail.com>
2020-10-29 02:40:42 -0700

    fixed the image folder

---
+++ src/pages/portfolio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Projects from '../components/Projects ';
+
+function Portfolio(props){
+
+    return(
+<div>
+    <Projects />
+</div>
+    );
+}
+
+export default Portfolio;
